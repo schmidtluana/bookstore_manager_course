0f3b18bd645fd37ad0252d4cda4f5a73dff5f1a7
Luana Schmidt <luana.caroline.schmidt@gmail.com>
2021-07-06 21:26:50 -0300

    First commit

---
+++ src/main/java/com/luanaschmidt/booksstoremanager/BooksstoremanagerApplication.java
@@ -0,0 +1,13 @@
+package com.luanaschmidt.booksstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksstoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksstoremanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luanaschmidt/booksstoremanager/BooksstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanaschmidt.booksstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
